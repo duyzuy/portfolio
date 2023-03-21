85059dcb20243dbeeca845691f0787dc115e0791
vu truong duy <vutruongduy2109@gmail.com>
2023-03-21 19:12:17 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,174 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import useMouse from "@react-hook/mouse-position";
+import { motion } from "framer-motion";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [cursorText, setCursorText] = useState("");
+  const [cursorVariant, setCursorVariant] = useState("default");
+  const ref = React.useRef(null);
+  const [lst, setlast] = useState<{ x: number | null; y: number | null }>({
+    x: 0,
+    y: 0,
+  });
+  const mouse = useMouse(ref, {
+    enterDelay: 0,
+    leaveDelay: 1000,
+  });
+
+  let mouseXPosition = lst.x;
+  let mouseYPosition = lst.y;
+
+  if (mouse.x !== null) {
+    mouseXPosition = mouse.clientX;
+  }
+
+  if (mouse.y !== null) {
+    mouseYPosition = mouse.clientY;
+  }
+  useEffect(() => {
+    if (mouse.x === null || mouse.y === null) {
+      setlast(() => ({
+        x: mouse.x,
+        y: mouse.y,
+      }));
+    }
+  }, [mouse]);
+
+  const variants = {
+    wrapper: {
+      opacity: 1,
+      height: 10,
+      width: 10,
+      fontSize: "16px",
+      backgroundColor: "#1e91d6",
+      x: (mouseXPosition && mouseXPosition) || 0,
+      y: (mouseYPosition && mouseYPosition) || 0,
+      transition: {
+        type: "spring",
+        mass: 0.6,
+      },
+    },
+    default: {
+      opacity: 0,
+      height: 10,
+      width: 10,
+      fontSize: "16px",
+      backgroundColor: "#1e91d6",
+      x: (mouseXPosition && mouseXPosition) || 0,
+      y: (mouseYPosition && mouseYPosition) || 0,
+      transition: {
+        type: "spring",
+        mass: 0.6,
+      },
+    },
+    project: {
+      opacity: 1,
+      backgroundColor: "#fff",
+      color: "#000",
+      height: 80,
+      width: 80,
+      fontSize: "18px",
+      x: (mouseXPosition && mouseXPosition - 32) || 0,
+      y: (mouseYPosition && mouseYPosition - 32) || 0,
+      transition: {
+        type: "spring",
+        mass: 0.6,
+      },
+    },
+    contact: {
+      opacity: 1,
+      backgroundColor: "#FFBCBC",
+      color: "#000",
+      height: 64,
+      width: 64,
+      fontSize: "32px",
+      x: (mouseXPosition && mouseXPosition - 48) || 0,
+      y: (mouseYPosition && mouseYPosition - 48) || 0,
+    },
+  };
+
+  const spring = {
+    type: "spring",
+  };
+
+  function projectEnter() {
+    setCursorText("View");
+    setCursorVariant("project");
+  }
+
+  function projectLeave() {
+    setCursorText("");
+    setCursorVariant("wrapper");
+  }
+
+  function contactEnter() {
+    setCursorText("👋");
+    setCursorVariant("contact");
+  }
+
+  function contactLeave() {
+    setCursorText("");
+    setCursorVariant("wrapper");
+  }
+  const wrapperEnter = () => {
+    setCursorText("");
+    setCursorVariant("wrapper");
+  };
+  const wrapperLeave = () => {
+    setCursorText("");
+    setCursorVariant("default");
+  };
+
+  function buttonEnter() {
+    setCursorText("Click");
+    setCursorVariant("project");
+  }
+  function buttonLeave() {
+    setCursorText("view");
+    setCursorVariant("project");
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      <div
+        className="container"
+        ref={ref}
+        onMouseEnter={wrapperEnter}
+        onMouseLeave={wrapperLeave}
+      >
+        <motion.div
+          variants={variants}
+          className="circle"
+          animate={cursorVariant}
+          transition={spring}
         >
-          Learn React
-        </a>
-      </header>
+          <span className="cursorText">{cursorText}</span>
+        </motion.div>
+        <div
+          className="project"
+          onMouseEnter={projectEnter}
+          onMouseLeave={projectLeave}
+        >
+          <span
+            className="projectName"
+            onMouseEnter={buttonEnter}
+            onMouseLeave={buttonLeave}
+          >
+            Discord Onboarding
+          </span>
+        </div>
+        <div className="contactWrapper">
+          <div
+            className="contact"
+            onMouseEnter={contactEnter}
+            onMouseLeave={contactLeave}
+          >
+            Want to Chat?
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
